0f509dd72b170fefdc942590a1468fedf4c2e658
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2026-05-27 18:33:49 +0200

    create the mod and simple build script

---
+++ readme.md
@@ -0,0 +1,9 @@
+## Building the mod (linux)
+
+[godotpcktool](https://github.com/hhyyrylainen/GodotPckTool) is required.
+
+```shell
+./scripts/build.sh
+```
+
+Mod and the meta file will be generated in /target directory.
---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+mkdir ./target
+rm ./target/*
+godotpcktool \
+  --pack ./target/KillTheTower.pck \
+  --action add --remove-prefix staging \
+  --file staging/KillTheTower
+cp ./KillTheTower.json ./target/KillTheTower.json
