0722b9ac9c8fcd179a543a4301d135db5a3ab56c
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2026-06-19 15:39:35 +0200

    update build process to include building for steam workshop

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,37 @@
+#!/bin/bash
+
+publish_steam=false
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --publish-steam-workshop)
+            publish_steam=true
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            shift
+            ;;
+    esac
+done
+
 mkdir ./target
-rm ./target/*
+rm target/*
+rm -r steam/target/*
 godotpcktool \
-  --pack ./target/KillTheTower.pck \
+  --pack target/KillTheTower.pck \
   --action add --remove-prefix staging \
   --file staging/KillTheTower
-cp ./KillTheTower.json ./target/KillTheTower.json
+cp ./KillTheTower.json target/KillTheTower.json
+
+mkdir -p steam/target/
+mkdir -p steam/target/content
+cp -r steam/meta/* steam/target
+cp -r target/KillTheTower.* steam/target/content
+
+if $publish_steam; then
+  echo "Publishing the mod to steam workshop"
+  ./steam/uploader/ModUploader upload -w steam/target
+fi
 
 cd target && zip -r KillTheTower.zip .
---
+++ steam/uploader/uploader.md
@@ -0,0 +1,2 @@
+Get latest uploader from [here](https://github.com/megacrit/sts2-mod-uploader/releases), unzip it in this
+directory . You should see `ModUploader` binary.
